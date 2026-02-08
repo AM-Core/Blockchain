0a5cbe6cc79f6926297330b077126b3b4291ef5b
AmirHossien Karimi <amirhkz1384@gmail.com>
2026-02-08 15:23:06 +0330

    Bump assembly info hashes and sourcelink
    Update build-generated assembly metadata and sourcelink references across projects. AssemblyInformationalVersion and AssemblyInfoInputs.cache values were updated for Application, ConsoleApp, DataStructures, Domain, DomainService, IO, and Tests, and ConsoleApp sourcelink now points to a new commit; corresponding obj/bin artifacts were regenerated. Also include a trivial whitespace edit in Application/CommandParser.cs.

--- Blockchain/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Blockchain/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+145bd8126387d33212306a82b56eafc87156dbc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70051fddeaeb60177ecad213899aefefae7c87e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Blockchain/IO/obj/Debug/net8.0/IO.AssemblyInfo.cs
+++ Blockchain/IO/obj/Debug/net8.0/IO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+145bd8126387d33212306a82b56eafc87156dbc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70051fddeaeb60177ecad213899aefefae7c87e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("IO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
